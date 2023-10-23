e6efe5a724fe6dfe0b7b56144d012e049e5487c1
Bhavani-5122004 <127508858+Bhavani-5122004@users.noreply.github.com>
2023-10-23 18:09:41 +0530

    Added basic template

--- client.c
+++ client.c
@@ -1,6 +1,7 @@
 #include "header.h"
 
-int main() {
+int main()
+{
     int client_sock;
     struct sockaddr_in client_addr;
     socklen_t addr_size;
@@ -8,12 +9,13 @@ int main() {
     // Create a socket and connect to the naming server
     connect_to_naming_server("127.0.0.1", &client_sock, &client_addr);
 
-    while (1) {
+    while (1)
+    {
         int option = -1;
         printf("\nDo you want to:\n");
         printf("Connect to a storage server     (enter 1)\n");
-        printf("Create an Empty File/Directory  (enter 2)\n");
-        printf("Delete a File/Directory         (enter 3)\n");
+        printf("Delete a File/Directory         (enter 2)\n");
+        printf("Create an Empty File/Directory  (enter 3)\n");
         printf("Copy Files/Directories          (enter 4)\n");
         printf("Edit File                       (enter 5)\n");
         printf("Read File                       (enter 6)\n");
@@ -22,68 +24,71 @@ int main() {
 
         scanf("%d", &option);
 
-        if (option == 1) {
-            if (send(client_sock, "1", strlen("1"), 0) == -1) {
+        if (option == 1)
+        {
+            if (send(client_sock, "1", strlen("1"), 0) == -1)
+            {
                 perror("[-]Send error");
                 exit(1);
             }
         }
-        else if (option == 2) {
-            if (send(client_sock, "2", strlen("2"), 0) == -1) {
+        else if (option == 2)
+        {
+            if (send(client_sock, "2", strlen("2"), 0) == -1)
+            {
                 perror("[-]Send error");
                 exit(1);
             }
         }
-        else if (option == 3) {
-            
-            if (send(client_sock, "3", strlen("3"), 0) == -1) {
+        else if (option == 3)
+        {
+
+            char option_client[2] = "3";
+            option_client[strlen(option_client)] = '\0';
+
+// Send option to NS
+            if (send(client_sock, option_client, sizeof(option_client), 0) == -1)
+            {
                 perror("[-]Send error");
                 exit(1);
             }
 
             char storage_server_num[10];
-           int  num_storage_servers = 10;
+            int num_storage_servers = 10;
             printf("Enter the storage server number: ");
             scanf("%s", storage_server_num);
-           
- if (atoi(storage_server_num) > num_storage_servers)
+
+            if (atoi(storage_server_num) > num_storage_servers)
             {
                 printf("Invalid storage server number\n");
                 continue;
-            }
-            else{
-                if (send(client_sock, storage_server_num, sizeof(storage_server_num), 0) == -1) {
-                perror("[-]Send error");
-                exit(1);
-            }
             }
 
             char path[MAX_FILE_PATH];
             printf("Enter the path: ");
             scanf("%s", path);
 
-
-            
-
-            if (send(client_sock, path, sizeof(path), 0) == -1)
-                printf("[-]Send error\n");
-                
             char create_option[10];
 
             printf("Do you want to\n");
             printf("Create an empty file       (Enter 1)\n");
             printf("Create an empty directory  (Enter 2)\n");
             scanf("%s", create_option);
-
-
-
+// Send storage sever number to the NS
+            if (send(client_sock, storage_server_num, sizeof(storage_server_num), 0) == -1)
+            {
+                perror("[-]Send error");
+                exit(1);
+            }
+// Send the path to NS
+            if (send(client_sock, path, sizeof(path), 0) == -1)
+                printf("[-]Send error\n");
+// Send whether you want to create a file or directory to NS
             if (send(client_sock, create_option, sizeof(create_option), 0) == -1)
                 printf("[-]Send error\n");
-            
-            printf(" ");
-       
         }
-         else {
+        else
+        {
             printf("Invalid option\n");
         }
     }
--- storageserver.c
+++ storageserver.c
@@ -13,147 +13,34 @@ int main()
     // Connect to SS
     connect_to_NS_from_SS(&sock, &addr, "127.0.0.1", 5566);
 
-    // Recieving the command number from the naming server
-    // while (1)
-    // {
-
-    // char file_path[1000];
-    // char command[1000];
-    // int received;
-    //     if ((received = recv(sock, &command, sizeof(command), 0)) == -1)
-    //     {
-    //         printf("Error recieving data\n");
-    //         exit(0);
-    //     }
-    //     else
-    //     {
-    //         command[received] = '\0';
-    //     }
-
-    //     if (strcmp(command, "connect") == 0)
-    //     {
-
-    //         // send data to naming server
-    //         printf("Enter port for communication with naming server: \n");
-    //         int server_port;
-    //         scanf("%d", &server_port);
-    //         printf("Enter port for communication with client: \n");
-    //         int client_port;
-    //         scanf("%d", &client_port);
-
-    //         ss_send *data = (ss_send *)malloc(sizeof(ss_send));
-    //         strcpy(data->ip_addr, ip);
-    //         data->client_port = client_port;
-    //         data->server_port = server_port;
-    //         data->files_and_dirs = NULL;
-    //         printf("Data sent to naming server.\n");
-
-    //         // send data to naming server
-    //         int s = send(sock, data, sizeof(ss_send), 0);
-
-    //         close(sock);
-    //         connect_to_naming_server(ip, &sock, &addr);
-    //     }
-
-    //     else if (strcmp(command, "create") == 0)
-    //     {
-
-    //         // Getting the file path
-    //    if ((received = recv(sock, &file_path, sizeof(file_path), 0)) == -1)
-    // {
-    //     printf("Error recieving data\n");
-    //     exit(0);
-    // }
-    // else
-    // {
-    //     file_path[received] = '\0';
-    // }
-    // char* temp = (char*)malloc(sizeof(char)*100);
-    // strcpy(temp,file_path);
-    //  char option[10];
-    //   if ((received = recv(sock, &option, sizeof(option), 0)) == -1)
-    // {
-    //     printf("Error recieving data\n");
-    //     exit(0);
-    // }
-    // else
-    // {
-    //     option[received] = '\0';
-    // }
-
-    // // If option is 1, create a file, if option is 2, create a directory
-    // if(strcmp(option,"1")==0){
-    //     create_file(temp);
-    // }
-    // else if(strcmp(option,"2")==0){
-    //     create_directory(temp);
-    // }
-    // // Sending success message
-    // int sent = send(sock, "done",sizeof("done"), 0);
-    //     }
-
-    //     else if (strcmp(command, "delete") == 0)
-    //     {
-
-    //         // Getting the file path
-    //        if ((received = recv(sock, &file_path, sizeof(file_path), 0)) == -1)
-    //     {
-    //         printf("Error recieving data\n");
-    //         exit(0);
-    //     }
-    //     else
-    //     {
-    //         file_path[received] = '\0';
-    //     }
-
-    //     char* temp = (char*)malloc(sizeof(char)*100);
-    //     strcpy(temp,file_path);
-
-    //  char option[10];
-    //   if ((received = recv(sock, &option, sizeof(option), 0)) == -1)
-    // {
-
-    //     printf("Error recieving data\n");
-    //     exit(0);
-    // }
-    // else
-    // {
-    //     option[received] = '\0';
-    // }
-
-    //     // If option is 1, delete a file, if option is 2, delete a directory
-    //     if(strcmp(option,"1")==0){
-    //         delete_file(temp);
-    //     }
-    //     else if(strcmp(option,"2")==0){
-    //         delete_directory(temp);
-    //     }
-    //     // Sending success message
-    //     int sent = send(sock, "done",sizeof("done"), 0);
-    //     }
-    // }
-
     addr_size = sizeof(client_addr);
-    //    Accept incoming connections
-    client_sock = accept(sock, (struct sockaddr *)&client_addr, &addr_size);
-    if (client_sock == -1)
-    {
-        perror("[-] Accept error");
-        exit(0);
-    }
-    
+
+    // Defining variables for sending and recieving
+    char file_path[MAX_FILE_PATH];
+    char command[2];
+    int received;
+    int i = 0;
     while (1)
     {
 
-        // Defining variables for sending and recieving
-        char file_path[MAX_FILE_PATH];
-        char command[2];
-        int received;
+        client_sock = accept(sock, (struct sockaddr *)&client_addr, &addr_size);
+        if (client_sock == -1)
+        {
+            perror("[-] Accept error");
+            exit(0);
+        }
 
         if ((received = recv(client_sock, &command, sizeof(command), 0)) == -1)
         {
             printf("Error recieving data\n");
             exit(0);
+        }
+        else if (received == 0)
+        {
+            // The client has closed the connection, so break out of the loop
+            printf("Client disconnected.\n");
+            close(client_sock);
+            break;
         }
         else
         {
@@ -180,6 +67,7 @@ int main()
 
             char *temp = (char *)malloc(sizeof(char) * 1000);
             strcpy(temp, file_path);
+
             char option[10];
 
             if ((received = recv(client_sock, &option, sizeof(option), 0)) == -1)
@@ -192,7 +80,7 @@ int main()
                 option[received] = '\0';
             }
 
-            // If option is 1, create a file, if option is 2, create a directory
+            // // If option is 1, create a file, if option is 2, create a directory
             if (strcmp(option, "1") == 0)
             {
                 create_file(temp);
